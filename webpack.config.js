4422cfedb4849d7f8870a9ff8552f8a240237331
Akshaytho <114489458+Akshaytho@users.noreply.github.com>
2025-08-24 12:01:18 +0530

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    port: 3000,
+    proxy: {
+      '/': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      },
+    },
+    historyApiFallback: true,
+  },
+};
